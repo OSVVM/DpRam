a5de821cadd43253df29d42a74e802cfa0df82f0
Jim Lewis <jim@synthworks.com>
2025-06-19 23:08:47 -0700

    Update test TimeOut and NoChecks detection

--- testbench/TbDpRam_BasicReadWrite.vhd
+++ testbench/TbDpRam_BasicReadWrite.vhd
@@ -68,15 +68,12 @@ begin
 
     -- Wait for test to finish
     WaitForBarrier(TestDone, 35 ms) ;
-    AlertIf(now >= 35 ms, "Test finished due to timeout") ;
-    AlertIf(GetAffirmCount < 1, "Test is not Self-Checking");
-    
     
     TranscriptClose ; 
     -- Printing differs in different simulators due to differences in process order execution
     -- AffirmIfTranscriptsMatch(PATH_TO_VALIDATED_RESULTS) ;
 
-    EndOfTestReports ; 
+    EndOfTestReports(TimeOut => (now >= 35 ms)) ; 
     std.env.stop ; 
     wait ; 
   end process ControlProc ; 
